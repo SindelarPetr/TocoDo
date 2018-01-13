401692bab8fdd71510789d18bf7ffa76e48e7b13
Petr Šindelář <petr.sinde@gmail.com>
2018-01-13 20:39:15 +0100

    Merged Days and RepeatType

--- TocoDo/TocoDo/Models/HabitModel.cs
+++ TocoDo/TocoDo/Models/HabitModel.cs
@@ -11,9 +11,7 @@ namespace TocoDo.Models
 	public enum HabitType { Daylong, Unit }
 
 	[Flags]
-	public enum Days { Mon = 1, Tue = 2, Wed = 4, Thu = 8, Fri = 16, Sat = 32, Sun = 64 }
-
-	public enum RepeatType { Days, Weeks, Months, Years }
+	public enum RepeatType { Mon = 1, Tue = 2, Wed = 4, Thu = 8, Fri = 16, Sat = 32, Sun = 64, Weeks = 128, Months = 256, Years = 512 }
 
 	public class HabitModel
 	{
@@ -22,9 +20,25 @@ namespace TocoDo.Models
 		public HabitType HabitType { get; set; }
 		public string Title { get; set; }
 		public string Description { get; set; }
+
+		#region Repeating
+		/// <summary>
+		/// Scheduling will work like this:
+		/// There is a StartDate which is the first day to start calculating in which exact days the habit should be active.
+		/// </summary>
 		public DateTime? StartDate { get; set; }
+
+		/// <summary>
+		/// Describes how is it with exact days for repeating. We can specify days of the week, or every week, month or years.
+		/// </summary>
 		public RepeatType RepeatType { get; set; }
+
+		/// <summary>
+		/// Specifies how many times should be the task repeated.
+		/// </summary>
 		public short RepeatNumber { get; set; }
+		#endregion
+
 		public int DailyFillingCount { get; set; }
 		public bool IsRecommended { get; set; }
 
