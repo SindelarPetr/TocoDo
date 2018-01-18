36abc45f8f38b067b1bcb0273ab521518d664048
Petr Šindelář <petr.sinde@gmail.com>
2018-01-18 19:22:55 +0100

    Fixed resource files

--- TocoDo/TocoDo/Properties/Resources.Designer.cs
+++ TocoDo/TocoDo/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace TocoDo {
+namespace TocoDo.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace TocoDo {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TocoDo.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TocoDo.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
