77c8e8f3575b9b66cf1728d52c951c38c368913a
Petr Šindelář <petr.sinde@gmail.com>
2018-01-22 23:10:50 +0100

    Styles reorder, new look in progress

---
+++ TocoDo/TocoDo/Views/Global/FrameWrapper.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TocoDo.Views.Global
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class FrameWrapper : ContentView
+	{
+		public FrameWrapper ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
