dae7c7ccd54c794a66c833bfb78e3e12777b9b1b
Petr Šindelář <petr.sinde@gmail.com>
2018-01-23 11:34:11 +0100

    Added test trigger actions

---
+++ TocoDo/TocoDo/TriggerActions/TestEnterAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Plugin.CrossPlatformTintedImage.Abstractions;
+using Xamarin.Forms;
+
+namespace TocoDo.TriggerActions
+{
+	public class TestEnterAction : TriggerAction<TintedImage>
+	{
+		protected override void Invoke(TintedImage sender)
+		{
+			
+		}
+	}
+}
