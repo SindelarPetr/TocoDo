90b79b5b627c98aa95486d297a814f22adc78a5e
Petr Šindelář <petr.sinde@gmail.com>
2018-01-24 16:20:41 +0100

    Added possibility to modify FrameWrapper header background color

--- TocoDo/TocoDo/Views/Global/FrameWrapper.xaml.cs
+++ TocoDo/TocoDo/Views/Global/FrameWrapper.xaml.cs
@@ -15,20 +15,31 @@ namespace TocoDo.Views.Global
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class FrameWrapper : Frame
 	{
+		#region Backing fields
 		public static BindableProperty InnerContentProperty = BindableProperty.Create(nameof(InnerContent), typeof(View), typeof(View));
 		public static BindableProperty HeaderProperty = BindableProperty.Create(nameof(Header), typeof(string), typeof(string), string.Empty);
+		public static BindableProperty HeaderBackgroundColorProperty = BindableProperty.Create(nameof(HeaderBackgroundColor), typeof(Color), typeof(Color), Color.Gray);
+		#endregion
 
+		#region Properties
 		public View InnerContent
 		{
-			get => (View) GetValue(InnerContentProperty);
+			get => (View)GetValue(InnerContentProperty);
 			set => SetValue(InnerContentProperty, value);
 		}
 
 		public string Header
 		{
-			get => (string) GetValue(HeaderProperty);
+			get => (string)GetValue(HeaderProperty);
 			set => SetValue(HeaderProperty, value);
 		}
+
+		public Color HeaderBackgroundColor
+		{
+			get => (Color) GetValue(HeaderBackgroundColorProperty);
+			set => SetValue(HeaderBackgroundColorProperty, value);
+		}
+		#endregion
 
 		public FrameWrapper()
 		{
