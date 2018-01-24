bd2e8bd6fe275867eec7dc296800b234248e22c2
Petr Šindelář <petr.sinde@gmail.com>
2018-01-24 22:58:28 +0100

    Created a picker which resizes itself according to the selected item

---
+++ TocoDo/TocoDo/Controls/ResizablePicker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TocoDo.Controls
+{
+    public class ResizablePicker : Picker
+    {
+	    public ResizablePicker()
+	    {
+		    SelectedIndexChanged += (a, b) => InvalidateMeasure();
+	    }
+    }
+}
