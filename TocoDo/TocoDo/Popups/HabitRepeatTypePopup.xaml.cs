d718640b294941633e6ee23029c2759498cb2ffd
Petr Šindelář <petr.sinde@gmail.com>
2018-01-24 16:22:18 +0100

    Added basic layout for Habit repeater

--- TocoDo/TocoDo/Popups/HabitRepeatTypePopup.xaml.cs
+++ TocoDo/TocoDo/Popups/HabitRepeatTypePopup.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Rg.Plugins.Popup.Pages;
+using TocoDo.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,6 +13,14 @@ namespace TocoDo.Popups
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class HabitRepeatTypePopup : PopupPage
 	{
+		public static BindableProperty SelectedRepeatTypeProperty = BindableProperty.Create(nameof(SelectedRepeatType), typeof(RepeatType), typeof(RepeatType), RepeatType.Days);
+
+		public RepeatType SelectedRepeatType
+		{
+			get => (RepeatType) GetValue(SelectedRepeatTypeProperty);
+			set => SetValue(SelectedRepeatTypeProperty, value);
+		}
+
 		public HabitRepeatTypePopup ()
 		{
 			InitializeComponent ();
