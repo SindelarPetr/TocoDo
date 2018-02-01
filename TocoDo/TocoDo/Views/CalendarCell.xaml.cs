f2a78832e9307c3cfd13dbdfc7af53c14fe71fe3
Petr Šindelář <petr.sinde@gmail.com>
2018-02-01 14:00:44 +0100

    Added horizontal bullet points

---
+++ TocoDo/TocoDo/Views/CalendarCell.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TocoDo.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CalendarCell : ContentView
+	{
+		public static BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(string));
+
+		public string Text
+		{
+			get => (string) GetValue(TextProperty);
+			set => SetValue(TextProperty, value);
+		}
+
+		public CalendarCell ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
