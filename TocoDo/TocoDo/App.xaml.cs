52c55abaac1a87d298a0d74e23b01b1160c46d43
Petr Šindelář <petr.sinde@gmail.com>
2018-03-07 16:26:16 +0100

    Reformatted App.xml.cs

--- TocoDo/TocoDo/App.xaml.cs
+++ TocoDo/TocoDo/App.xaml.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using Microsoft.AppCenter;
-using Microsoft.AppCenter.Analytics;
-using Microsoft.AppCenter.Crashes;
 using TocoDo.BusinessLogic;
 using TocoDo.BusinessLogic.DependencyInjection;
 using TocoDo.BusinessLogic.DependencyInjection.Models;
 using TocoDo.BusinessLogic.Helpers;
 using TocoDo.BusinessLogic.Services;
-using TocoDo.UI.DependencyInjection;
-using TocoDo.UI.Pages.Main;
 using TocoDo.UI.Properties;
 using Xamarin.Forms;
 
@@ -16,32 +11,21 @@ namespace TocoDo.UI
 {
 	public partial class App : Application
 	{
+		#region Static
+
 		public static Action BarColorChanged;
 
-		public App()
-		{
-			MyLogger.WriteStartMethod();
+		#endregion
 
-			MyLogger.WriteLine("Before initialize component");
-			InitializeComponent();
-			MyLogger.WriteLine("After initialize component");
+		#region Properties
 
-			var main = new MainPage();
-			MyLogger.WriteInMethod();
+		public IPersistance       Persistance  => (IPersistance) Resources[AppStrings.Persistance];
+		public IModelFactory      ModelFactory => (IModelFactory) Resources[AppStrings.ModelFactory];
+		public IDateTimeProvider  DateProvider => (IDateTimeProvider) Resources[AppStrings.DateTimeProvider];
+		public INavigationService Navigation   => (INavigationService) Resources[AppStrings.Navigation];
+		public ITaskService       TaskService  => (ITaskService) Resources[AppStrings.TaskService];
+		public IHabitService      HabitService => (IHabitService) Resources[AppStrings.HabitService];
 
-			var navigationPage = new NavigationPage(main);
-			MainPage           = navigationPage;
-
-			MyLogger.WriteEndMethod();
-		}
-
-		public IPersistance Persistance => (IPersistance) Resources[AppStrings.Persistance];
-		public IModelFactory ModelFactory => (IModelFactory) Resources[AppStrings.ModelFactory];
-		public IDateTimeProvider DateProvider => (IDateTimeProvider) Resources[AppStrings.DateTimeProvider];
-		public INavigationService Navigation => (INavigationService) Resources[AppStrings.Navigation];
-		public ITaskService TaskService => (ITaskService) Resources[AppStrings.TaskService];
-		public IHabitService HabitService => (IHabitService) Resources[AppStrings.HabitService];
-	
 		public Color ColorPrimary
 		{
 			get => (Color) Resources["ColorPrimary"];
@@ -60,7 +44,26 @@ namespace TocoDo.UI
 			set => Resources["ColorPrimaryDark"] = value;
 		}
 
-		
+		#endregion
+
+		public App()
+		{
+			MyLogger.WriteStartMethod();
+
+			MyLogger.WriteLine("Before initialize component");
+			InitializeComponent();
+			MyLogger.WriteLine("After initialize component");
+
+			var main = new MainPage();
+			MyLogger.WriteInMethod();
+
+			var navigationPage = new NavigationPage(main);
+			MainPage           = navigationPage;
+
+			MyLogger.WriteEndMethod();
+		}
+
+
 		protected override async void OnStart()
 		{
 			MyLogger.WriteStartMethod();
@@ -78,6 +81,7 @@ namespace TocoDo.UI
 				MyLogger.WriteException(e);
 				throw;
 			}
+
 			MyLogger.WriteEndMethod();
 		}
 	}
